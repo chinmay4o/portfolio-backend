56fbb1e4cd267ec60fd01fcd86c9b6db08673f08
surve4404 <surve4404@gmail.com>
2021-08-31 12:01:49 +0530

    modi tranporter

--- router/contact.js
+++ router/contact.js
@@ -4,13 +4,15 @@ const router = express.Router();
 import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth : {
-        user: "surve4407@gmail.com",
-        pass : process.env.MAIL_PASS,
-    }
-});
+
+const transport = nodemailer.createTransport({
+    host: "in-v3.mailjet.com",
+    port: 587,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
 
 router.post("/f1", async (req, res) => {
   const { name, email, message } = req.body;
@@ -21,7 +23,7 @@ router.post("/f1", async (req, res) => {
   try {
     transporter.sendMail({
       to: "chinmayinbox8@gmail.com",
-      from: "surve4407@gmail.com",
+      from: process.env.EMAIL,
       subject: `new portfolio contact`,
       html: `
         <h1>new portfolio contact</h1>
@@ -38,3 +40,24 @@ router.post("/f1", async (req, res) => {
 });
 
 export const routerMail = router;
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth : {
+//         user: "surve4407@gmail.com",
+//         pass : process.env.MAIL_PASS,
+//     }
+// });
+
+
+
+    // transport.sendMail({
+    //   to: user.email,
+    //   from: process.env.EMAIL,
+    //   subject: `Signup Successful`,
+    //   html: `
+    //   <h1>Welcome, ${user.name} ${user.surname} To Dark Services</h1>
+    //   <h5>Click on <a href="https://url-shortner-server-1.herokuapp.com/api/url/verify?token=${token}">Link</a> , To Activate Account.</h5>
+    //   <p>Doing The Above Step Help US :)</p>
+    //   `,
+    // });
